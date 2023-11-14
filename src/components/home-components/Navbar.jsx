570105fd3a6b617cb5852021dc738d07728dc5be
Andrea <andrea.saitta.as@gmail.com>
2023-11-14 21:22:52 +0100

    .

--- src/components/home-components/Navbar.jsx
+++ src/components/home-components/Navbar.jsx
@@ -34,10 +34,10 @@ const Navbar = ({ hamburger, event }) => {
           </Link>
           <div className="hamburger">
             <input type="checkbox" id="checkbox" onClick={event}/>
-            <label for="checkbox" class="toggle">
-              <div class="bars" id="bar1"></div>
-              <div class="bars" id="bar2"></div>
-              <div class="bars" id="bar3"></div>
+            <label htmlFor="checkbox" className="toggle">
+              <div className="bars" id="bar1"></div>
+              <div className="bars" id="bar2"></div>
+              <div className="bars" id="bar3"></div>
             </label>
           </div>
         </div>
