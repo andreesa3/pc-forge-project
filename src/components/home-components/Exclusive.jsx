1d7e4df96956955fde70d1a6f1c39d73cc622be4
Nicola Marci <nicola.marci1996@gmail.com>
2023-11-20 12:40:21 +0100

    update css/responsive  exlusive/calltoaction

--- src/components/home-components/Exclusive.jsx
+++ src/components/home-components/Exclusive.jsx
@@ -1,59 +1,81 @@
 import Button from "./Button";
 
+import cpuIcon from "../../assets/icons/componentsIcon/cpu-icon.svg";
+import gpuIcon from "../../assets/icons/componentsIcon/gpu-icon.svg";
+import ssdIcon from "../../assets/icons/componentsIcon/ssd-icon.svg";
+import ramIcon from "../../assets/icons/componentsIcon/ram-icon.svg";
+
 const Exclusive = () => {
   return (
     <section className="exclusive-build">
       <div className="exclusive-build-wrapper wrapper">
-          <div className="container-sx">
-            <h1>Lorem Ipsum</h1>
-            <div className="build-list">
-              <p>Lorem Ipsum</p>
-              <p>Lorem Ipsum</p>
-              <p>Lorem Ipsum</p>
+        <div className="container-sx">
+          <h2>Lorem Ipsum</h2>
+          <div className="container-sx-pc">
+            <img src="./src/assets/images/pc1.jpg" alt="pc-build" />
+          </div>
+          <div className="build-list">
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni
+              eaque, numquam ea unde repudiandae nam accusantium facilis
+              voluptatem architecto illum?
+            </p>
+          </div>
+          <div className="price">
+            <p>2500,00€</p>
+            <Button text="Click me" theme="btn-purple" />
+          </div>
+        </div>
+        <div className="container-center">
+          <div className="pc">
+            <img src="./src/assets/images/pc1.jpg" alt="pc-build" />
+          </div>
+        </div>
+        <div className="container-dx">
+          <div className="info">
+            <div className="info-card">
+              <div>
+                <img src={cpuIcon} alt="cpu-icon" />
+              </div>
+              <div>
+                <h3>Lorem Ipsum</h3>
+                <p>Lorem Ipsum dolor sit amet</p>
+              </div>
             </div>
-            <div className="price">
+            <div className="info-card">
+              <div>
+                <img src={gpuIcon} alt="gpu-icon" />
+              </div>
+              <div>
+                <h3>Lorem Ipsum</h3>
+                <p>Lorem Ipsum dolor sit amet</p>
+              </div>
+            </div>
+            <div className="info-card">
+              <div>
+                <img src={ssdIcon} alt="ssd-icon" />
+              </div>
+              <div>
+                <h3>Lorem Ipsum</h3>
+                <p>Lorem Ipsum dolor sit amet</p>
+              </div>
+            </div>
+            <div className="info-card">
+              <div>
+                <img src={ramIcon} alt="ram-icon" />
+              </div>
+              <div>
+                <h3>Lorem Ipsum</h3>
+                <p>Lorem Ipsum dolor sit amet</p>
+              </div>
+            </div>
+            <div className="info-price">
               <p>2500,00€</p>
               <Button text="Click me" theme="btn-purple" />
             </div>
           </div>
-          <div className="container-center">
-            <div className="pc">
-              <img src="./scr/assets/images/pc1.jpg" alt="pc-build" />
-              </div>
-          </div>
-          <div className="container-dx">
-            <div className="info">
-              <div>
-                <img src="#" alt="#" />
-              </div>
-              <div>
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem Ipsum dolor sit amet</p>
-              </div>
-              <div>
-                <img src="#" alt="#" />
-              </div>
-              <div>
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem Ipsum dolor sit amet</p>
-              </div>
-              <div>
-                <img src="#" alt="#" />
-              </div>
-              <div>
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem Ipsum dolor sit amet</p>
-              </div>
-              <div>
-                <img src="#" alt="#" />
-              </div>
-              <div>
-                <h2>Lorem Ipsum</h2>
-                <p>Lorem Ipsum dolor sit amet</p>
-              </div>
-            </div>
-          </div>
         </div>
+      </div>
     </section>
   );
 };
