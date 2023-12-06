4ad64943f8418a8bd0a2b0c6b6f1cace15226390
Luca Campo <138501100+lucaprogrammer25@users.noreply.github.com>
2023-12-06 21:22:36 +0100

    fix chat css

--- client/src/components/builder/chat.jsx
+++ client/src/components/builder/chat.jsx
@@ -1,27 +1,26 @@
-import { useState } from "react";
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import { openai } from "./openai";
 import InputForm from "./inputform";
 import Conversation from "./Conversation";
-import { useCallback } from "react";
 
 export default function Chat() {
   const [conversations, setConversations] = useState([]);
   const [aiMessage, setAiMessage] = useState("");
   const [userMessage, setUserMessage] = useState("");
+  const chatContainerRef = useRef(null);
 
   const handleSubmit = useCallback(
     async (e) => {
       e.preventDefault();
 
-      setConversations((prev) => {
-        return [
-          ...prev,
-          {
-            message: userMessage,
-            isHuman: true,
-          },
-        ];
-      });
+      setConversations((prev) => [
+        ...prev,
+        {
+          message: userMessage,
+          isHuman: true,
+        },
+      ]);
+
       setUserMessage("");
 
       const stream = await openai.chat.completions.create({
@@ -36,15 +35,13 @@ export default function Chat() {
         setAiMessage((prev) => prev + part.choices[0].delta.content);
 
         if (part.choices[0].finish_reason === "stop") {
-          setConversations((prev) => {
-            return [
-              ...prev,
-              {
-                message: streamedMessage,
-                isHuman: false,
-              },
-            ];
-          });
+          setConversations((prev) => [
+            ...prev,
+            {
+              message: streamedMessage,
+              isHuman: false,
+            },
+          ]);
 
           setAiMessage("");
           break;
@@ -56,8 +53,15 @@ export default function Chat() {
     [userMessage]
   );
 
+  useEffect(() => {
+    //Aggiunto per far scorrere la chat automaticamente
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+    }
+  }, [conversations, aiMessage]);
+
   return (
-    <div className="Chat">
+    <div className="Chat" ref={chatContainerRef}>
       <Conversation conversations={conversations} aiMessage={aiMessage} />
       <InputForm
         userMessage={userMessage}
